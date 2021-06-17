17cf854af84a867501d1751f424dc05759107c42
Rafael Mello <rafael.ferreira.sobral@gmail.com>
2021-06-17 14:01:26 +1000

    Fetched API, Button to categories created

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-fetch('https://fakestoreapi.com/products/1')
+fetch('https://fakestoreapi.com/products/categories')
     .then(res=>res.json())
-    .then(json=>console.log(json))
+    .then(json=> {
+        document.querySelector("button").addEventListener("click", (e) => {
+            json.forEach((value, index) => {
+                document.querySelector("#categories").innerHTML += `<br><button>${value}</button><br>`
+            })
+        })
+    })
+    
