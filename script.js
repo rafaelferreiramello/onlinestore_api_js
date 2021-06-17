1157a6389e2240544209790d01113e535e0e69cc
Rafael Mello <rafael.ferreira.sobral@gmail.com>
2021-06-17 15:43:08 +1000

    Fetched products from API, Products cards added

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 fetch('https://fakestoreapi.com/products/categories')
     .then(res=>res.json())
     .then(json=> {
-        document.querySelector("button").addEventListener("click", (e) => {
-            json.forEach((value, index) => {
-                document.querySelector("#categories").innerHTML += `<br><button>${value}</button><br>`
+        categoriesDiv = document.querySelector("#categories")
+        json.forEach(value => {
+            button = document.createElement("button")
+            button.classList.add("btn", "btn-primary", "btn-sm", "rounded-pill", "m-3")
+            button.innerText = value.toUpperCase()
+            button.addEventListener("click", () => {
+                fetch(`https://fakestoreapi.com/products/category/${value}`)
+                .then(resp => resp.json())
+                .then(json => {
+                        json.forEach((value, index) => {
+                            document.querySelector(".product").innerHTML += 
+                                `<div class="card" style="width: 18rem;">
+                                    <img src="${value.image}" class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">${value.title}</h5>
+                                        <p class="card-text">${value.description}</p>
+                                        <a href="#" class="btn btn-primary">Add to the cart</a>
+                                    </div>
+                                </div>`
+                        })
+                })
+
             })
+            categoriesDiv.appendChild(button)
         })
     })
-    
+
